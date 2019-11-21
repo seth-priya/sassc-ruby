b9f60ab850a2b2837fc1b607de66e533a03e31b0
Priya Seth <sethp@us.ibm.com>
2019-11-21 08:43:47 +0530

    Update extconf.rb

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -19,12 +19,6 @@ if enable_config('static-stdlib', false)
   $LDFLAGS << ' -static-libgcc -static-libstdc++'
 end
 
-# Set to false when building binary gems
-if enable_config('march-tune-native', true)
-  $CFLAGS << ' -march=native -mtune=native'
-  $CXXFLAGS << ' -march=native -mtune=native'
-end
-
 if enable_config('lto', true)
   $CFLAGS << ' -flto'
   $CXXFLAGS << ' -flto'
